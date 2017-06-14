d7bafb2d20fcea6345a0c48bab5cf586947fafb0
tc-nguyen <tc.nguyen@live.com>
2017-06-14 15:01:18 +0200

    Ajout drag & drop jquery ui

--- public/script.js
+++ public/script.js
@@ -18,7 +18,11 @@ $("document").ready(function() {
 	  }
 	
   });
-  
+ 
+	$(function(){
+		$(".list-group").sortable();
+		$(".list-group").disableSelection();
+	});
   
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Lyon&APPID=9b754f1f40051783e4f72c176953866e&units=metric&lang=fr", function(data) {
     console.log(data);
