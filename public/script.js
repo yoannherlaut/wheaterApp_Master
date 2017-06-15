6fa4f084a1b8c30ffce888a2981b766918c824c2
tc-nguyen <tc.nguyen@live.com>
2017-06-15 08:57:47 +0200

    Sortable update et ajout id dans li

--- public/script.js
+++ public/script.js
@@ -1,31 +1,38 @@
-$("document").ready(function() {
+$("document").ready(function () {
 
-  $("li").each(function(i) {
-    console.log(i);
-  });
+	/*$("li").each(function(i) {
+	  console.log(i);
+	});*/
 
 
-  $("#btn-valide").click(function(){ 
-	if ($("#city").val().length > 2) {
-  
-    //$("#btn-valide").remove();
-    //$(this).fadeOut().delay(5000).fadeIn();
-  		}
-	  else {
-    	event.preventDefault();
-		
-		$("#messageErreur").html( "<center><strong><p>Merci de saisir un nom de ville supérieur à deux caractères !!!</p></strong></center>" );		  
-	  }
-	
-  });
- 
-	$(function(){
-		$(".list-group").sortable();
+	$("#btn-valide").click(function () {
+		if ($("#city").val().length > 2) {
+
+			//$("#btn-valide").remove();
+			//$(this).fadeOut().delay(5000).fadeIn();
+		} else {
+			event.preventDefault();
+
+			$("#messageErreur").html("<center><strong><p>Merci de saisir un nom de ville supérieur à deux caractères !!!</p></strong></center>");
+		}
+
+	});
+
+	$(function () {
+
+		$(".list-group").sortable(
+		{
+			update: function( event,ui )
+			{console.log($(".list-group").sortable("toArray"));}
+		}
+		);
 		$(".list-group").disableSelection();
 	});
-  
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Lyon&APPID=9b754f1f40051783e4f72c176953866e&units=metric&lang=fr", function(data) {
-    console.log(data);
-  });
-  
+	
+	
+
+	/*$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Lyon&APPID=9b754f1f40051783e4f72c176953866e&units=metric&lang=fr", function(data) {
+	  console.log(data);
+	});*/
+
 })
